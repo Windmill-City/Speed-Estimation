8b3a9a07ca13732fb15417c20f96de0709ec335f
Windmill_City <1449182174@qq.com>
2023-06-04 22:51:04 +0800

    seems work!

--- Track.py
+++ Track.py
@@ -1,9 +1,9 @@
-from ultralytics import YOLO
-from ultralytics.yolo.engine.results import Boxes, Results
+from collections import deque
 
 import cv2
-import torch
 import numpy as np
+import torch
+from ultralytics import YOLO
 
 
 class Camera:
@@ -27,7 +27,8 @@ class Camera:
         self.dx = cmos_w / pix_w
         self.dy = cmos_h / pix_h
         # Image Origin in pixel
-        self.origin = [pix_w / 2, pix_h / 2]
+        self.origin_w = pix_w / 2
+        self.origin_h = pix_h / 2
         self.height = height
         self.pitch = np.deg2rad(pitch)
 
@@ -42,9 +43,8 @@ class Camera:
             gc_xy: ground coord
         """
         # Relocate origin
-        xy -= torch.tensor(self.origin, device=xy.device, dtype=xy.dtype)
-        wi = xy[:, 0]
-        hi = xy[:, 1]
+        wi = xy[:, 0] - self.origin_w
+        hi = xy[:, 1] - self.origin_h
         # Angle between optical y-axis and imaging point ray
         beta = torch.atan(hi * self.dy / self.f)
         # Evaluate ground coord
@@ -76,7 +76,7 @@ class GroundCoordBoxes:
 
             Args:
                 xyxy (numpy.ndarray): pixel coordinate of the bounding box
-            
+
             Returns:
                 (numpy.ndarray): Bottom center coordinate of the bounding box
             """
@@ -89,9 +89,46 @@ class GroundCoordBoxes:
             return xy
 
         self.boxes = boxes
-        
+
         xy = pixel_ground_coord(self.boxes.xyxy)
         self.coords = camera.to_ground_coord(xy)
+
+
+class TrackResult:
+    """
+    TrackResult object
+    """
+
+    def __init__(self, id, cls, fps, window=1) -> None:
+        self.id = id
+        self.cls = cls
+        self.interval = 1 / fps
+
+        self.last_coord = None
+        self.last_xyxy = None
+        # Speed filter cache
+        self.speeds = deque(maxlen=int(fps * window))
+
+    def update(self, xyxy, coord):
+        self.last_xyxy = xyxy
+
+        if self.last_coord is not None:
+            speed = (coord - self.last_coord) / self.interval
+            self.speeds.append(speed.numpy())
+        self.last_coord = coord
+
+        return self
+
+    @property
+    def speed(self):
+        return np.mean(self.speeds, axis=0)
+
+    @property
+    def xyxy(self):
+        return self.last_xyxy
+
+    def __str__(self) -> str:
+        return f"id: {self.id}, cls: {self.cls}"
 
 
 class Tracker:
@@ -102,12 +139,12 @@ class Tracker:
     def __init__(self, video, filters=["person"], device=0) -> None:
         self.model = YOLO("yolov8n.pt")
 
-        self.video = cv2.VideoCapture(video)
+        _video = cv2.VideoCapture(video)
 
-        self.fps = self.video.get(cv2.CAP_PROP_FPS)
+        self.fps = _video.get(cv2.CAP_PROP_FPS)
 
-        self.frame_w = self.video.get(cv2.CAP_PROP_FRAME_WIDTH)
-        self.frame_h = self.video.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        self.frame_w = _video.get(cv2.CAP_PROP_FRAME_WIDTH)
+        self.frame_h = _video.get(cv2.CAP_PROP_FRAME_HEIGHT)
         self.camera = Camera(2000, 10, self.frame_w, self.frame_h)
 
         names = list(self.model.names.values())
@@ -118,12 +155,45 @@ class Tracker:
             stream=True,
         )
 
+        self.tracks = {}
+        self.orig_img = None
+
+    def result(self):
+        for result in self.pre_result:
+            boxes = result.boxes
+            gcs = GroundCoordBoxes(boxes, self.camera)
+            self.orig_img = result.orig_img
+
+            def update(self, gcs):
+                boxes = gcs.boxes
+
+                ids = boxes.id.numpy().astype(np.int32) if boxes.id is not None else np.empty(0)
+                cls = boxes.cls.cpu().numpy()
+
+                xxyy = boxes.xyxy
+                coords = gcs.coords
+
+                for id, cl, xyxy, coord in zip(ids, cls, xxyy, coords):
+                    track = (
+                        self.tracks[id]
+                        if id in self.tracks
+                        else TrackResult(id, result.names[cl], self.fps)
+                    )
+                    self.tracks[id] = track.update(xyxy, coord)
+
+                # Remove tracks that no more exist in current frame
+                self.tracks = dict(
+                    filter(lambda item: item[0] in ids, self.tracks.items())
+                )
+
+            update(self, gcs)
+            yield self.tracks.values()
+
     def plot(
-        result,
+        self,
         line_width=None,
         font_size=None,
         font="Arial.ttf",
-        labels=True,
         boxes=True,
         show=True,
     ):
@@ -131,7 +201,6 @@ class Tracker:
         Plots the detection results on an input RGB image.
 
         Args:
-            labels (bool): Whether to plot the label of bounding boxes.
             boxes (bool): Whether to plot the bounding boxes.
             show (bool): Whether to show the annotated image
 
@@ -139,23 +208,36 @@ class Tracker:
             (numpy.ndarray): A numpy array of the annotated image.
         """
         from copy import deepcopy
+
         from ultralytics.yolo.utils.plotting import Annotator, colors
 
-        names = result.names
-        annotator = Annotator(
-            deepcopy(result.orig_img), line_width, font_size, font, example=names
-        )
+        annotator = Annotator(deepcopy(self.orig_img), line_width, font_size, font)
+
+        if boxes:
+            for track in self.tracks.values():
+                label = str(track)
+                annotator.box_label(track.xyxy.squeeze(), label, color=colors(0, True))
+
+                if np.shape(track.speed) == (2,):
+                    speed = np.linalg.norm(track.speed)
+                    speed = np.round(speed, 0)
+                    
+                    xy = track.xyxy[[0, 3]].numpy().astype(np.int32)
+                    annotator.text(
+                        xy,
+                        f"speed: {speed}",
+                        txt_color=colors(0, True),
+                        box_style=True,
+                    )
 
         res = annotator.result()
-
         if show:
             cv2.imshow("result", res)
             cv2.waitKey(1)
-
         return res
 
 
 tracker = Tracker("Video.mp4")
-for result in tracker.pre_result:
-    boxes = result.boxes
-    GroundCoordBoxes(boxes, tracker.camera)
+
+for tracks in tracker.result():
+    tracker.plot()
